e69e534b90b153df9daea39b5513358a8efc3b8d
shuuuuuny <shuuuuuny@gmail.com>
2021-01-24 17:17:43 +0900

    rbenv recipe

--- roles/default.rb
+++ roles/default.rb
@@ -1,5 +1,17 @@
 execute 'echo hello world'
-execute 'echo $PATH'
+# execute 'echo $PATH'
 # execute 'export PATH=$PATH:/usr/local/bin'
 # execute 'mitamae version'
 execute '/usr/local/bin/mitamae version'
+
+node.reverse_merge!(
+  rbenv: {
+    user: 'root',
+    global: '3.0.0',
+    versions: %w[
+      3.0.0
+    ],
+  }
+)
+
+include_recipe "rbenv::user"
