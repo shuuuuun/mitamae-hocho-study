e7dc95770078e0a391bae80030234a795c3c0482
shuuuuuny <shuuuuuny@gmail.com>
2021-01-25 01:19:43 +0900

    fix rbenv

--- roles/default.rb
+++ roles/default.rb
@@ -1,17 +1,28 @@
 execute 'echo hello world'
 execute '/usr/local/bin/mitamae version'
 
+# rbenv
 node.reverse_merge!(
   rbenv: {
-    user: 'root',
     global: '3.0.0',
     versions: %w[
       3.0.0
     ],
   }
 )
-
-include_recipe "rbenv::user"
+include_recipe "rbenv::system"
+file "/etc/profile.d/rbenv.sh" do
+  mode "0644"
+  # content <<~EOS.chomp
+  #   export RBENV_ROOT=/usr/local/rbenv
+  #   export PATH="${RBENV_ROOT}/bin:${PATH}"
+  #   eval "$(rbenv init -)"
+  # EOS
+  content \
+    'export RBENV_ROOT=/usr/local/rbenv' "\n" \
+    'export PATH="${RBENV_ROOT}/bin:${PATH}"' "\n" \
+    'eval "$(rbenv init -)"'
+end
 
 # mysql
 package 'https://dev.mysql.com/get/mysql80-community-release-el7-3.noarch.rpm' do
