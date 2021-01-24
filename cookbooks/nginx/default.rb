9c183e9217fd701231adfd87c03ddadaf6aaee5c
shuuuuuny <shuuuuuny@gmail.com>
2021-01-24 22:48:23 +0900

    fix nginx rpm check

--- cookbooks/nginx/default.rb
+++ cookbooks/nginx/default.rb
@@ -1,5 +1,5 @@
 package 'http://nginx.org/packages/centos/7/noarch/RPMS/nginx-release-centos-7-0.el7.ngx.noarch.rpm' do
-  not_if 'rpm -q nginx-release-centos-7-0'
+  not_if 'rpm -q nginx'
 end
 
 package "nginx" do
